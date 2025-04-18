dc8c1c218552bbf11d00dc69c2838fbaac24b8c5
Jin <passawit53@gmail.com>
2025-04-18 14:58:45 +0700

    finished

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,11 +1,61 @@
 import { Router } from "express";
-
+import bcrypt from "bcrypt";
+import { db } from "../utils/db.js";
+import jwt from "jsonwebtoken";
 const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {
+    try {
+        const { username, password, firstName, lastName } = req.body;
 
-// 🐨 Todo: Exercise #3
-// ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        
+        const newUser = {
+            username,
+            password: hashedPassword,
+            firstName,
+            lastName
+        };
+        
+        const collection = db.collection("users");
+        await collection.insertOne(newUser);
+
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        console.error("Register error:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
+authRouter.post("/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        const collection = db.collection("users");
+        const user = await collection.findOne({ username });
+
+        if (!user) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id, username: user.username  }, process.env.SECRET_KEY, { expiresIn: "1h" });
+
+        res.status(200).json({ message: "Login successful" , token });
+    } catch (error) {
+        console.error("Login error:", error);
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
 
 export default authRouter;
